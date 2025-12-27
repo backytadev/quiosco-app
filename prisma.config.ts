f14c30a57ea396cccdb947fa39de534f1600be7a
backyta <kevi.baca18@gmail.com>
2025-12-27 10:51:36 -0500

    fix: :bug: fix config prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'prisma/config';
 export default defineConfig({
   migrations: {
     path: 'prisma/migrations',
+    seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
   },
   datasource: {
     url: process.env.DATABASE_URL!,
