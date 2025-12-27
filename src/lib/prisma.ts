e290b13d907bed867084f7e6f116e581149b4c6e
backyta <kevi.baca18@gmail.com>
2025-12-27 10:49:09 -0500

    fix: :bug: fix config and engine

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL!,
+const prismaClientSingleton = () => {
+  return new PrismaClient({
     log: ['query'],
   });
+};
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
